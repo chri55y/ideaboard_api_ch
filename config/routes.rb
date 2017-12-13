b8f0b80cc2881dcf58eba58b0964d0339be61d33
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2017-12-13 14:47:20 -0600

    wrap resources in api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # get 'ideas/index'
-  # use resources instead of match routes
-  resources :ideas
+  # namespacing --> best practices to version apis
+  namespace :api  do
+    namespace :v1   do
+      resources :ideas
+    end
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
